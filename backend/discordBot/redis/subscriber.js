9972e35ac13e905f7a670fe374df26cf2399a5c6
DanyLenovo <tolozadaniel5@gmail.com>
2026-06-13 02:49:25 -0400

    feat: añadi una subcripcion "a2sFollowPlayersAlerts" pero aun incompleta

--- backend/discordBot/redis/subscriber.js
+++ backend/discordBot/redis/subscriber.js
@@ -20,6 +20,15 @@ async function initRedisSubscriber(subscriber, redis, client) {
         }
     });
 
+    //
+    subscriber.subscribe('a2sFollowPlayersAlerts', (err, count) => {
+        if (err) {
+          console.error('Error al suscribirse a subscriber:', err);
+        } else {
+          console.log(`Suscrito a ${count} canal(es) en subscriber.`);
+        }
+    });
+
 
     subscriber.on('message', async (channel, message) => {
         console.log(`Mensaje recibido en ${channel}: ${message}`);
@@ -29,6 +38,28 @@ async function initRedisSubscriber(subscriber, redis, client) {
             channel === 'adressChangePlayerCount'
         ) {
             changeInfoGameServerHandler(channel, message, redis, client);
+        }
+
+        else if(channel === 'a2sFollowPlayersAlerts'){
+            // Manejar mensajes para alertas de jugadores seguidos
+
+            /*
+            {
+              "playersAlerts": [
+                {
+                  "name": "Player1",
+                  "action": "joined",
+                  "timestamp": "2024-06-01T12:00:00Z",
+                  "description": "razon del alert"
+                  "priority": "alta/baja"
+                  ""
+                },
+                
+              "address": "
+            }
+
+            */
+            message = JSON.parse(message);  
         }
     });
 }
